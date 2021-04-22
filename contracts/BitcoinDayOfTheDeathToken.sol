dc2a9d1cf094f8bc2e1692f63b9a98f2057d2a0d
unknown <antonio.salazar.emrsn@gmail.com>
2021-04-22 00:12:24 -0500

    Added the gitignore file Added the btcDayOfTheDeathNFT.json file which contains the metadata of our NFT

--- contracts/BitcoinDayOfTheDeathToken.sol
+++ contracts/BitcoinDayOfTheDeathToken.sol
@@ -1,2 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+
+import '../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
+
+contract BTCmxDEATH is ERC721 {
+    
+
+}
